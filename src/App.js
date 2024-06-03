5872e36ea3d73ac86d2a003fc88470063ad91681
Patrick Hamer <patrick.hamer@interlate.com>
2024-06-03 14:10:07 +1000

    adding routing using createBrowserRouter

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import { useEffect, useState, useReducer } from "react";
-
+import { createBrowserRouter, createRoutesFromElements, Route, RouterProvider, Outlet } from "react-router-dom";
 
 import { ProductList } from "./components/ProductList";
 // import ProductListClass from "./components/ProductListClass";
@@ -13,52 +13,78 @@ import { GlobalContext } from "./components/utils/globalStateContext";
 import globalReducer from "./components/reducers/globalReducer";
 
 function App() {
-  const [isLoading, setIsLoading] = useState(true)
-  const [selectedItem, setSelectedItem] = useState(null)
+  const [isLoading, setIsLoading] = useState(true);
+  // const [selectedItem, setSelectedItem] = useState(null);
 
   const initialState = {
-    loggedInUserName: '',
-    token: ''
-  }
+    loggedInUserName: "",
+    token: "",
+  };
 
-  const [store, dispatch] = useReducer(globalReducer, initialState)
+  const [store, dispatch] = useReducer(globalReducer, initialState);
 
-  function setItem(item) {
-    setSelectedItem(item)
-  }
+  // function setItem(item) {
+  //   setSelectedItem(item);
+  // }
 
   setTimeout(() => {
-    setIsLoading(false)
-  }, 2000)
+    setIsLoading(false);
+  }, 2000);
 
-  useEffect(()=> {
-    const username = localStorage.getItem("username")
-    const token = localStorage.getItem("token")
-    if(username && token) {
+  useEffect(() => {
+    const username = localStorage.getItem("username");
+    const token = localStorage.getItem("token");
+    if (username && token) {
       dispatch({
-        type: 'setLoggedInUsername',
-        data: username
-      })
+        type: "setLoggedInUsername",
+        data: username,
+      });
       dispatch({
-        type: 'setToken',
-        data: token
-      })
+        type: "setToken",
+        data: token,
+      });
     }
-  }, [])
+  }, []);
 
   return (
     <>
-    {isLoading === true ? <CircularIndeterminate />: 
+      {isLoading === true ? (
+        <CircularIndeterminate />
+      ) : (
         <div className="App">
-        <GlobalContext.Provider value={{store, dispatch}}>
-        <NavBar />
-        <Login />
-        <ProductList setItem={setItem}/>
-        <ProductInfo item ={selectedItem}/>
-        <AddProduct />
-        <Cart />
-        </GlobalContext.Provider>
-      </div>}
+          <GlobalContext.Provider value={{ store, dispatch }}>
+            <RouterProvider router={router} />
+          </GlobalContext.Provider>
+        </div>
+      )}
+    </>
+  );
+}
+
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route path="/" element={<MainPage/>}>
+      <Route path="login" element={<Login />} />
+    </Route>
+  )
+)
+
+function MainPage() {
+  const [selectedItem, setSelectedItem] = useState(null);
+
+  function setItem(item) {
+    setSelectedItem(item);
+  }
+
+  return (
+    <>
+      <NavBar />
+      <Outlet />
+      {/* <Login />
+      <ProductList setItem={setItem} />
+      <ProductInfo item={selectedItem} />
+      <AddProduct />
+      <Cart /> */}
     </>
   );
 }
